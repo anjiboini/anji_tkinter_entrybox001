3054d408d4426798892d65ab0b0d0efd9460abfe
home <sdsad>
2020-08-30 13:39:17 +0530

    entry box code added

--- entry box2.py
+++ entry box2.py
@@ -1,1 +1,26 @@
-anji
+from tkinter import  *
+from tkinter import *
+from PIL import ImageTk,Image
+import mysql.connector
+import csv
+from tkinter import ttk
+
+root = Tk()
+root.title('well come to India thanudatabase')
+root.iconbitmap('D:/anji_python software/python/anji tkinter projects/resources/thanu1.ico')
+root.geometry('600x400')
+
+def myclick():
+    hello = "Hello " + e.get()
+    mylabel=Label(root,text=hello)
+    mylabel.pack(pady=10)
+
+e = Entry(root, width = 50)
+e.pack(padx=10,pady=10)
+
+myButton = Button(root,text="Enter you Name", command =myclick)
+myButton.pack(pady=10)
+
+
+
+root.mainloop()
